507fe02b012d182b0904729f95089cb9099498db
edmayen <ed_mayen@hotmail.com>
2020-06-07 23:05:15 -0500

    First commit

---
+++ ejercicio2.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+var producto1: String = "Televisiones Full HD"
+var cantidad1: Int = 18 
+var descuentoProd1: String = "25%"
+var entrega1: Bool = true
+
+var producto2: String = "Smartphones"
+var cantidad2: Int = 14
+var descuentoProd2: String = "15%"
+var entrega2: Bool = true
+
+var producto3: String = "Muebles"
+var cantidad3: Int = 6
+var descuentoProd3: String = "20%"
+var entrega3: Bool = false
+
+var producto4: String = "Bolsa de detergente de kilo y medio"
+var cantidad4: Int = 12
+var descuentoProd4: String = "15%"
+var entrega4: Bool = true
+
+var producto5: String = "Refrigerador"
+var cantidad5: Int = 4
+var descuentoProd5: String = "15%"
+var entrega5: Bool = false
+
+var producto6: String = "Latas de mayonesa de 450 gramos"
+var cantidad6: Int = 23
+var descuentoProd6: String = "30%"
+var entrega6: Bool = true
+
+var producto7: String = "Manzanas"
+var cantidad7: Float = 18.5
+var descuentoProd7: String = "25%"
+var entrega7: Bool = true
+
+var oracion1: String = "Hay \(cantidad1) unidades de \(producto1) con un \(descuentoProd1) de descuento. Disponibilidad inmediata: \(entrega1)"
+var oracion2: String = "Hay \(cantidad2) unidades de \(producto2) con un \(descuentoProd2) de descuento. Disponibilidad inmediata: \(entrega2)"
+var oracion3: String = "Hay \(cantidad3) unidades de \(producto3) con un \(descuentoProd3) de descuento. Disponibilidad inmediata: \(entrega3)"
+var oracion4: String = "Hay \(cantidad4) unidades de \(producto4) con un \(descuentoProd4) de descuento. Disponibilidad inmediata: \(entrega4)"
+var oracion5: String = "Hay \(cantidad5) unidades de \(producto5) con un \(descuentoProd5) de descuento. Disponibilidad inmediata: \(entrega5)"
+var oracion6: String = "Hay \(cantidad6) unidades de \(producto6) con un \(descuentoProd6) de descuento. Disponibilidad inmediata: \(entrega6)"
+var oracion7: String = "Hay \(cantidad7) unidades de \(producto7) con un \(descuentoProd7) de descuento. Disponibilidad inmediata: \(entrega7)"
+
+
+print(oracion1)
+print(oracion2)
+print(oracion3)
+print(oracion4)
+print(oracion5)
+print(oracion6)
+print(oracion7)
+
+
+
+
+
+
